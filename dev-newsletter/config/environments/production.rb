aff389f115f11f1ce379bad0422b6bba996cd32f
Radhika Radhakrishnan <radhika.jec@gmail.com>
2016-09-12 09:39:17 -0400

    Initial commit

--- dev-newsletter/config/environments/production.rb
+++ dev-newsletter/config/environments/production.rb
@@ -54,9 +54,9 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "sample_app_#{Rails.env}"
+  # config.active_job.queue_name_prefix = "dev-newsletter_#{Rails.env}"
   config.action_mailer.perform_caching = false
-  config.force_ssl = true
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
